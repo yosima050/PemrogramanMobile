14d12c05e649a87637370ee579aff4683200a15a
yosima050 <byosep050@gmail.com>
2026-04-07 17:23:13 +0700

    #05 Praktikum 4 Langkah 1

--- Pertemuan5/hello_world/README.md
+++ Pertemuan5/hello_world/README.md
@@ -2,18 +2,10 @@
 
 A new Flutter project.
 
-![SS](images/01.png)
+(images/01.png)
 
-## Getting Started
+## Praktikum 4: Menerapkan Widget Dasar
 
-This project is a starting point for a Flutter application.
+### Hasil Langkah 1:
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
-- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![Langkah 1](images/02.png)
--- Pertemuan5/hello_world/lib/main.dart
+++ Pertemuan5/hello_world/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -102,9 +103,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const MyTextWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
