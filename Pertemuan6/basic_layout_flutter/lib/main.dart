cbdc178e2fa7501395fc3f082b83c75645337aed
yosima050 <byosep050@gmail.com>
2026-04-21 22:58:02 +0700

    #06 Basic Layout Flutter: 'Step 4: Column'

--- Pertemuan6/basic_layout_flutter/lib/main.dart
+++ Pertemuan6/basic_layout_flutter/lib/main.dart
@@ -8,27 +8,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Layout Step 3',
+      title: 'Flutter Layout Step 4',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Step 3: Row')),
+        appBar: AppBar(title: const Text('Step 4: Column')),
         body: Center(
-          child: Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 padding: const EdgeInsets.all(10),
-                color: Colors.red,
-                child: const Text('Red'),
+                color: Colors.orange,
+                child: const Text('Top'),
               ),
               Container(
                 padding: const EdgeInsets.all(10),
-                color: Colors.green,
-                child: const Text('Green'),
+                color: Colors.purple,
+                child: const Text('Middle'),
               ),
               Container(
                 padding: const EdgeInsets.all(10),
-                color: Colors.blue,
-                child: const Text('Blue'),
+                color: Colors.teal,
+                child: const Text('Bottom'),
               ),
             ],
           ),
