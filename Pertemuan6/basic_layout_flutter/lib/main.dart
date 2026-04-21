d836acaa43ce0061c5777bb152bc6e32faf7d806
yosima050 <byosep050@gmail.com>
2026-04-21 22:59:46 +0700

    #06 Basic Layout Flutter: 'Step 6: Button Column'

--- Pertemuan6/basic_layout_flutter/lib/main.dart
+++ Pertemuan6/basic_layout_flutter/lib/main.dart
@@ -5,33 +5,42 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color, size: 32),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Layout Step 5',
+      title: 'Flutter Layout Step 6',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Step 5: Expanded & Image')),
-        body: Row(
-          children: [
-            Expanded(
-              child: Image.network(
-                'images/batu1.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-            Expanded(
-              child: Image.network(
-                'images/batu2.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-            Expanded(
-              child: Image.network(
-                'images/batu3.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
+        appBar: AppBar(title: const Text('Step 6: Button Column')),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(Colors.blue, Icons.call, 'CALL'),
+              _buildButtonColumn(Colors.blue, Icons.near_me, 'ROUTE'),
+              _buildButtonColumn(Colors.blue, Icons.share, 'SHARE'),
+            ],
+          ),
         ),
       ),
     );
